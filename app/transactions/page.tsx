513523b51e4826a53397bd892fb7afd89c619a0f
camilladavyla <lendaukoro12games@gmail.com>
2025-08-07 21:18:29 -0300

    feat: add expenses per category component

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,4 +1,4 @@
-import { db } from "../_lib/prisma";
+import { db } from "@/app/_lib/prisma";
 import { DataTable } from "../_components/ui/data-table";
 import { transactioncolumns } from "./_columns";
 import AddTransaction from "../_components/add-transaction-button";
@@ -7,32 +7,31 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 const TransactionsPage = async () => {
-    const {userId} = await auth()
-    if (!userId){
-     redirect('/login')
-    }
-    // Acessar as transações do meu banco de dados
-    const transactions = await db.transaction.findMany({
-        where: {
-            userId,
-        },
-    });
-    return (
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  // Acessar as transações do meu banco de dados
+  const transactions = await db.transaction.findMany({
+    where: {
+      userId,
+    },
+  });
+  return (
     <>
-    <Navbar />
-    <div className="p-6 space-y-6">
+      <Navbar />
+      <div className="space-y-6 p-6">
         {/* TÍTULO E BOTÃO */}
-        <div className="flex w-full justify-between items-center ">
-            <h1 className="font-bold text-2xl">Transações</h1>
-            <AddTransaction />
+        <div className="flex w-full items-center justify-between">
+          <h1 className="text-2xl font-bold">Transações</h1>
+          <AddTransaction />
         </div>
         <div>
-            <DataTable columns={transactioncolumns} data={transactions}/>
+          <DataTable columns={transactioncolumns} data={transactions} />
         </div>
-    </div>
+      </div>
     </>
-    );
+  );
 };
- 
-export default TransactionsPage;
 
+export default TransactionsPage;

3d92a1f7e1fcad634b42388b24c23cc2c8595ac1
camilladavyla <lendaukoro12games@gmail.com>
2025-08-07 22:04:26 -0300

    feat: add last transaction component

--- app/_data/get-dashboard/index.ts
+++ app/_data/get-dashboard/index.ts
@@ -90,6 +90,11 @@ export const getDashboard = async (month: string) => {
       (Number(category._sum.amount) / Number(expensesTotal)) * 100,
     ),
   }));
+  const lastTransactions = await db.transaction.findMany({
+    where,
+    orderBy: { date: "desc" },
+    take: 10,
+  });
   return {
     balance,
     depositsTotal,
@@ -97,5 +102,6 @@ export const getDashboard = async (month: string) => {
     expensesTotal,
     typesPercentage,
     totalExpensePerCategory,
+    lastTransactions,
   };
 };
